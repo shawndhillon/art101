f5b2e43d1463e84706426e3d050e32527ea5c7b0
shawndhillon <gdhill11@ucsc.edu>
2025-04-21 15:48:56 -0700

    lab3

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,43 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Four in a Row
+// An implementation of the game
+// Shawn Dhillon <gdhill11@ucsc.edu>
+// April 2024
+
+// Print the board before each turn  
+    // Loop through each row  
+        // Loop through each column  
+            // Print X, O, or Space  
+            // Print a | between  
+        // Print a newline  
+        // Print a horizontal line after each row  
+        
+// Game loop  
+//Loop until board is full OR four in a row:  
+    // Accept user input  
+    // Prompt user for column choice  
+
+    // Validate user move  
+    // While chosen column is full:  
+        // Print "Choose another column"  
+        // Prompt user for new column choice  
+
+    // Place tile  
+    // Locate topmost empty row in chosen column  
+    // Place current player's tile (X/O)  
+
+    // Check for win  
+    // If four tiles in a row (horizontal/vertical/diagonal):  
+        // Print "Player {tile(X/O)} wins!"  
+        // Break loop/Return
+    
+    // Check for tie
+    // If all tiles filled
+      // Print("Tie Game")
+      // Break Loop/Return
+
+    // Switch players  
+    // Alternate current player (from X to O or O to X)  
+
 
 // Constants
 
