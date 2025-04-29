c6aec06571a12bc54611b417eab9eb94e9acf2d9
shawndhillon <gdhill11@ucsc.edu>
2025-04-29 11:48:42 -0700

    comments

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -6,19 +6,26 @@
 
 // Functions
 function sortUserName(){
+  // prompts user for a username and prints it to console
   var username = window.prompt("give your username to be alphabetized");
   console.log("username =", username);
 
+  // creates an array from the username splitting on every character
+  // prints it to console
   var namearray = username.split('');
   console.log("namearray =", namearray);
 
+  // sorts the name array and prints to console
   var namearraysort = namearray.sort();
   console.log("namearraysort =", namearraysort);
 
+  // turns the array back into a string and prints it to console
   var namesorted = namearraysort.join('');
   console.log("namesorted =", namesorted);
 
+  // returns the sorted string
   return namesorted;
 }
 
+// writes out the sorted name
 document.writeln("Your alphabetized named is: ", sortUserName(), "</br>")
