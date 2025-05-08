8560b93189e6220583e128e2d9514a49ad5b1e47
shawndhillon <gdhill11@ucsc.edu>
2025-05-08 12:13:36 -0700

    lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,19 @@
+
+/*
+   lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+   Requirements: jQuery must be loaded for this script to work
+   Author: Shawn Dhillon
+   Date: 2025
+*/
+
+
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>"
+   + "\xA0" + "Am I Special?"
+);
+
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
