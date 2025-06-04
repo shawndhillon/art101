1eab81afff2467b3b9850428f272feec97c766ec
shawndhillon <gdhill11@ucsc.edu>
2025-06-03 17:58:54 -0700

    stuffs

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -23,9 +23,9 @@ $(document).ready(function() {
       dataType : "json",
       // What do we do when the api call is successful
       //   all the action goes in here
-      success: function(comicObj) {
+      success: function(data) {
           // do stuff
-          
+          let comicObj = JSON.parse(data.contents)
           let title = comicObj.title;
           let img = comicObj.img;
           let content = comicObj.alt;
@@ -35,7 +35,7 @@ $(document).ready(function() {
             '<br>' + '<img  src="' + img + '"/>'  + '</div>'  );
 
           console.log(comicObj);
-          console.log(comicObj.content);
+          console.log(comicObj.img);
 
       
       },
@@ -49,3 +49,50 @@ $(document).ready(function() {
   });
 });
 
+
+$(document).ready(function() {
+  $("#act").click(function(){
+    // Generate a random number between 1 and 898 (as per Pokémon API)
+  let comic = Math.floor(Math.random() * 3096) + 1;
+
+  // Using the core $.ajax() method
+  $.ajax({
+      // The URL for the request (from the api docs)
+      url: "https://xkcd.com/info.0.json",
+      // The data to send (will be converted to a query string)
+      data: { 
+              // here is where any data required by the api 
+              //   goes (check the api docs)
+              // NA  
+            },
+      // Whether this is a POST or GET request
+      type: "GET",
+      // The type of data we expect back
+      dataType : "json",
+      // What do we do when the api call is successful
+      //   all the action goes in here
+      success: function(data) {
+          // do stuff
+          let comicObj = JSON.parse(data.contents)
+          let title = comicObj.title;
+          let img = comicObj.img;
+          let content = comicObj.alt;
+ 
+        
+         $("#output").html( '<h2>' + title + '</h2>' + content + '<div class = "photo-section" style = "justify-content: center;">' +
+            '<br>' + '<img  src="' + img + '"/>'  + '</div>'  );
+
+          console.log(comicObj);
+          console.log(comicObj.img);
+
+      
+      },
+      // What we do if the api call fails
+      error: function (jqXHR, textStatus, errorThrown) { 
+          // do stuff
+          console.log("Error:", textStatus, errorThrown);
+      }
+  })
+
+  });
+});
